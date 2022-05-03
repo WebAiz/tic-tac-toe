fca279bfe5928c58690e3cfc3dc4f7ba6f6a7013
aizada <aizada.turarova@gmail.com>
2022-05-03 21:20:25 +0600

    Improve code

--- Bot.mjs
+++ Bot.mjs
@@ -1,3 +1,5 @@
+import { PIECES } from "./constants.mjs";
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -11,27 +13,24 @@ export class Bot1 {
   }
 
   move(board) {
-    // console.log(
-    // 	"%c Talgat - board -> ",
-    // 	"background: #222; color: royalblue",
-    // 	board
-    // );
+    console.log(
+      "%c Talgat - board -> ",
+      "background: #222; color: royalblue",
+      board
+    );
     // let i = getRandomInt(0, 9);
     // while (board[i] !== 0) {
     // 	i = getRandomInt(0, 9);
     // }
-    // console.log(
-    // 	"%c Talgat - i -> ",
-    // 	"background: #222; color: royalblue",
-    // 	i
-    // );
+
+
     // return i;
-    let bestScore = -1000;
+    let bestScore = -10000;
     let bestMove;
     const newBoard = [...board];
     for (let i = 0; i < 9; i++) {
       if (newBoard[i] === 0) {
-        newBoard[i] = 1;
+        newBoard[i] = PIECES.X;
         const score = minimax(newBoard, 0, false);
         newBoard[i] = 0;
         if (score > bestScore) {
@@ -41,6 +40,10 @@ export class Bot1 {
       }
 
     }
+    console.log(
+      "%c Talgat - bestMove -> ",
+      "background: #222; color: royalblue",
+      bestMove);
     return bestMove;
   }
 }
@@ -64,32 +67,48 @@ export class Bot2 {
     console.log("%c Abat - i -> ", "background: #222; color: royalblue", i);
     return i;
 
+    // let bestScore = Number.MAX_VALUE;
+    // let bestMove;
+    // const newBoard = [...board];
+    // for (let i = 0; i < 9; i++) {
+    //   if (newBoard[i] === 0) {
+    //     newBoard[i] = PIECES.O;
+    //     const score = minimax(newBoard, 0, true);
+    //     newBoard[i] = 0;
+    //     if (score < bestScore) {
+    //       bestScore = score;
+    //       bestMove = i;
+    //     }
+    //   }
 
+    // }
+    // return bestMove;
   }
 }
 
-// [0 1 2] // [3 4 5] // [6 7 8] // horizontal
+
 function minimax(board, depth, isMax) {
   let score = checkWinner(board);
 
   // If Maximizer has won the game
   // return his/her evaluated score
-  if (score === 1)
+  if (score === PIECES.X)
     return score;
 
   // If Minimizer has won the game
   // return his/her evaluated score
-  if (score === -1)
+  if (score === PIECES.O)
     return score;
 
   if (score === "tie")
     return 0;
+
   // If this maximizer's move
   if (isMax) {
-    let best = -1000;
+    let best = -10000;
     for (let i = 0; i < 9; i++) {
       if (board[i] === 0) {
-        board[i] = 1;
+        board[i] = PIECES.X;
         best = Math.max(best, minimax(board,
           depth + 1, false));
         board[i] = 0;
@@ -100,10 +119,10 @@ function minimax(board, depth, isMax) {
 
   // If this minimizer's move
   else {
-    let best = 1000;
+    let best = 10000;
     for (let i = 0; i < 9; i++) {
       if (board[i] === 0) {
-        board[i] = -1;
+        board[i] = PIECES.O;
         best = Math.min(best, minimax(board,
           depth + 1, true));
         board[i] = 0;
@@ -114,43 +133,44 @@ function minimax(board, depth, isMax) {
 }
 
 function checkWinner(board) {
-  let winner = null; // [0 1 2] // [3 4 5] // [6 7 8] // horizontal
+  let winner = null;
 
+  // Row
   if (board[0] === board[1] && board[1] === board[2] && board[0] !== 0) {
-    if (board[0] === 1) winner = 1;
-    else winner = -1;
+    if (board[0] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
   if (board[3] === board[4] && board[4] === board[5] && board[3] !== 0) {
-    if (board[3] === 1) winner = 1;
-    else winner = -1;
+    if (board[3] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
   if (board[6] === board[7] && board[7] === board[8] && board[6] !== 0) {
-    if (board[6] === 1) winner = 1;
-    else winner = -1;
+    if (board[6] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
 
   // Vertical
   if (board[0] === board[3] && board[3] === board[6] && board[0] !== 0) {
-    if (board[0] === 1) winner = 1;
-    else winner = -1;
+    if (board[0] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
   if (board[1] === board[4] && board[4] === board[7] && board[1] !== 0) {
-    if (board[1] === 1) winner = 1;
-    else winner = -1;
+    if (board[1] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
   if (board[2] === board[5] && board[5] === board[8] && board[2] !== 0) {
-    if (board[2] === 1) winner = 1;
-    else winner = -1;
+    if (board[2] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
 
   // Diagonal
   if (board[0] === board[4] && board[4] === board[8] && board[0] !== 0) {
-    if (board[0] === 1) winner = 1;
-    else winner = -1;
+    if (board[0] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
   if (board[2] === board[4] && board[4] === board[6] && board[2] !== 0) {
-    if (board[2] === 1) winner = 1;
-    else winner = -1;
+    if (board[2] === PIECES.X) winner = PIECES.X;
+    else winner = PIECES.O;
   }
 
   let openSpots = 0;
